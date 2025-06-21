7af9b0ca477bcf2e50f033058ddda3240a2eae21
yumin1209 <55646526+yumin1209@users.noreply.github.com>
2025-06-21 23:31:42 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,52 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] submitted = new boolean[31];
 
-        int array[] = new int[30];
-        int array2[] = new int[28];
-        int array3[] = new int[2];
-        int index = 0;
-
-
-        for (int i = 0; i < 30; i++) {
-            array[i] = i + 1;
+        for (int i = 0; i < 28; i++) {
+            int num = Integer.parseInt(br.readLine());
+            submitted[num] = true;
         }
 
-        for (int i = 0; i < array2.length; i++) {
-            int num = sc.nextInt();
-            array2[i] = num;
-        }
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array2.length; j++) {
-                if (array[i] == array2[j]) {
-                    array[i] = 0;
-                }
+        for (int i = 1; i <= 30; i++) {
+            if (!submitted[i]) {
+                System.out.println(i);
             }
         }
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] != 0) {
-                array3[index++] = array[i];
-            }
-        }
-
-        int max = array3[0];
-        int min = array3[0];
-
-        for (int i = 0; i < array3.length; i++) {
-            if (array3[i] > max) {
-                max = array3[i];
-            }
-            if (array3[i] < min) {
-                min = array3[i];
-            }
-        }
-
-        System.out.println(min);
-        System.out.println(max);
     }
 }
 
