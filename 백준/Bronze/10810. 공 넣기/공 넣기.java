8aec9df5662891676cd0443b273845c1386e34fc
yumin1209 <55646526+yumin1209@users.noreply.github.com>
2025-06-21 18:03:17 +0900

    [Bronze III] Title: 공 넣기, Time: 212 ms, Memory: 18616 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,31 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int index = 0;
+        int x,y,z;
+
+        int array[] = new int[num1];
+
+        for (int i = 0; i < num2; i++) {
+            x = sc.nextInt();
+            y = sc.nextInt();
+            z = sc.nextInt();
+
+            for (int j = x; j <= y; j++) {
+                    array[j-1] = z;
+                }
+            }
+
+        for (int i = 0; i < num1; i++) {
+            System.out.print(array[i] + " ");
+
+        }
+        }
+    }
